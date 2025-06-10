c0adfc6fda8034b961cee171642a00a75525eb88
Sathishkumar Ramasamy <sathishkumar.r@actian.com>
2025-06-10 18:34:03 +0530

    API version

--- index.js
+++ index.js
@@ -1,11 +1,77 @@
 const express = require('express');
-const app = express();
-const PORT = 3000;
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello, Sathishkumar!</h1><p>This is your simple Express server.</p>');
+const app = express();
+app.use(bodyParser.json());
+
+// MySQL Connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
 });
 
+db.connect(err => {
+    if (err) throw err;
+    console.log('Connected to MySQL Database!');
+});
+
+// Routes
+
+// 1. Create a new user
+app.post('/users', (req, res) => {
+    const { name, email } = req.body;
+    const sql = 'INSERT INTO tbl_users (name, email) VALUES (?, ?)';
+    db.query(sql, [name, email], (err, result) => {
+        if (err) throw err;
+        res.send({ message: 'User created!', userId: result.insertId });
+    });
+});
+
+// 2. Get all users
+app.get('/users', (req, res) => {
+    const sql = 'SELECT * FROM tbl_users';
+    db.query(sql, (err, results) => {
+        if (err) throw err;
+        res.send(results);
+    });
+});
+
+// 3. Get a single user by ID
+app.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = 'SELECT * FROM tbl_users WHERE id = ?';
+    db.query(sql, [id], (err, result) => {
+        if (err) throw err;
+        res.send(result[0] || { message: 'User not found' });
+    });
+});
+
+// 4. Update a user
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body;
+    const sql = 'UPDATE tbl_users SET name = ?, email = ? WHERE id = ?';
+    db.query(sql, [name, email, id], (err, result) => {
+        if (err) throw err;
+        res.send({ message: 'User updated!' });
+    });
+});
+
+// 5. Delete a user
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = 'DELETE FROM tbl_users WHERE id = ?';
+    db.query(sql, [id], (err, result) => {
+        if (err) throw err;
+        res.send({ message: 'User deleted!' });
+    });
+});
+
+// Start the server
+const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is listening at http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
